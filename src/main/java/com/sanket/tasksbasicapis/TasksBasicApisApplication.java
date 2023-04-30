1776d972ce3cd47d05e2aad5f451488a7e882e55
sanketwakhare <sanketwakhare@gmail.com>
2023-04-30 20:50:09 +0530

    Initial commit

---
+++ src/main/java/com/sanket/tasksbasicapis/TasksBasicApisApplication.java
@@ -0,0 +1,13 @@
+package com.sanket.tasksbasicapis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksBasicApisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksBasicApisApplication.class, args);
+    }
+
+}
