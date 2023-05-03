932ab837eac55f7ee20acda7a72e6b87acc62f39
sanketwakhare <sanketwakhare@gmail.com>
2023-05-03 15:37:31 +0530

    add exceptions and handlers

---
+++ src/main/java/com/sanket/tasksbasicapis/tasks/exceptions/IllegalTaskArgumentException.java
@@ -0,0 +1,7 @@
+package com.sanket.tasksbasicapis.tasks.exceptions;
+
+public class IllegalTaskArgumentException extends IllegalArgumentException {
+    public IllegalTaskArgumentException(String message) {
+        super(message);
+    }
+}
